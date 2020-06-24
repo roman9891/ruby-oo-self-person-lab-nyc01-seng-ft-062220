b8ef56ead47895d99f2cbb1ca5607fdac2336cdd
orlandofat <orlandofat@gmail.com>
2020-06-24 11:31:53 -0400

    update

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,7 @@
 class Person 
     attr_accessor :bank_account, :happiness, :hygiene
     attr_reader :name
+    #attr_writer :happiness
     def initialize(name, bank_account = 25, happiness = 8, hygiene = 8) 
         @name = name
         @bank_account = bank_account
@@ -9,11 +10,42 @@ class Person
         @hygiene = hygiene
     end
 
-    def happiness
-        @happiness = 10
+    def happiness=(value)
+        @happiness = value
         if @happiness > 10
-            
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
         end
-        #return @happiness = 10
+        return @happiness 
+    end
+
+    def hygiene=(value)
+        @hygiene = value
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+        return @hygiene 
+
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(money)
+        @bank_account += money
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 end
