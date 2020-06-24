f920c13533cb84bb1adae5b0dff72fc085e824ed
roman9891 <roman9891@gmail.com>
2020-06-24 11:02:57 -0400

    pair project

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # your code goes here
+class Person 
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8) 
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness
+        @happiness = 10
+        if @happiness > 10
+            
+        end
+        #return @happiness = 10
+    end
+end
